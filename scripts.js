5b6033900c05802df6d086c117ff908db71bae1a
JTA Team <64577007+JTA15TM@users.noreply.github.com>
2020-08-10 21:08:12 +0300

    new site

---
+++ scripts.js
@@ -0,0 +1,27 @@
+onMenuClick = function(value)
+{
+    var redirect = document.getElementsByClassName("menu_item")[value].getAttribute("redirect");
+    window.location.href = redirect;
+}
+
+onload = function()
+{
+    //Body Setup
+    document.getElementsByTagName("body")[0].style.opacity = "100%";
+
+    //Content Setup
+    var height = window.screen.height;
+    var width = window.screen.height;
+
+    var content_height = "";
+
+    if(height > width)
+    {
+        content_height = ((height / 2) + (height / 12)) + "px";
+    }
+    else
+    {
+        content_height = ((width / 2) + (width / 12)) + "px";
+    }
+    document.getElementsByClassName("content")[0].style.height = content_height;
+}
